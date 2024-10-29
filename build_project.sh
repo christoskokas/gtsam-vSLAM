66bb3f7aba539ac9ef918d83d7d7b03ff29e3649
Christos Kokas <christos.kokas95@gmail.com>
2024-10-29 02:59:28 +0200

    update build_project.sh, working on debug only

--- build_project.sh
+++ build_project.sh
@@ -1,37 +1,25 @@
 echo "Downloading all apt-get packages needed ..."
 
-# sudo apt-get install cmake
+sudo apt-get install -y libglm-dev libglfw3-dev libpng-dev libjpeg-dev libboost-all-dev libopencv-dev python3-opencv libyaml-cpp-dev libsuitesparse-dev libeigen3-dev libatlas-base-dev cmake libgoogle-glog-dev libgflags-dev libpcl-dev
 
-# sudo apt-get install libgoogle-glog-dev libgflags-dev
+curl -s --compressed "https://koide3.github.io/ppa/ubuntu2004/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/koide3_ppa.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/koide3_ppa.gpg] https://koide3.github.io/ppa/ubuntu2004 ./" | sudo tee /etc/apt/sources.list.d/koide3_ppa.list
 
-# sudo apt-get install libatlas-base-dev
-
-# sudo apt-get install libeigen3-dev
-
-# sudo apt-get install libsuitesparse-dev
-
-# sudo apt-get install libyaml-cpp-dev
-
-# sudo apt install libopencv-dev python3-opencv
-
-# sudo apt-get install libboost-all-dev
-
-sudo apt-get install -y libglm-dev libglfw3-dev libpng-dev libjpeg-dev libboost-all-dev libopencv-dev python3-opencv libyaml-cpp-dev libsuitesparse-dev libeigen3-dev libatlas-base-dev cmake libgoogle-glog-dev libgflags-dev
-
-echo "Downloading and Building GTSAM ..."
+sudo apt update && sudo apt install -y libiridescence-dev
 
 mkdir packages
 cd packages
 
-git clone --recursive https://github.com/borglab/gtsam.git
+echo "Downloading and Building GTSAM ..."
+
+git clone --recursive --branch 4.2 --depth 1 https://github.com/borglab/gtsam.git
 
 #!bash
 cd gtsam
 mkdir build
 cd build
 sudo cmake ..
-sudo make -j8
-sudo make install
+sudo make install -j8
 
 
 cd ../../../
